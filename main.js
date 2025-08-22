205fe93612e76dcd36bf4c6e9480a89674bbad81
JurnalistManda <rafiamin7710@gmail.com>
2025-08-22 17:46:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+// let nama = 'rafi amin' // tipe string
+// let usia = 17 // tipe interger number
+// let tinggiBadan = 170.4 // Tipe double float coma di js menggunakan titik 
+// let beratBadan
+// let pacar = 1
+
+// beratBadan = 70
+
+// if(pacar == null ){
+//     pacar = 'belum punya'
+// } else {
+//     pacar = 'diah'
+// }
+
+// let saldoAwal = 1000000
+// let saldoTambahan = 800000
+// const hutang = 300000
+// const saldoAkhir = saldoAwal + saldoTambahan - hutang
+// // switch (pacar) {
+// //     case 1: 
+// //         pacar = "punya 1 aja"
+// //         break
+// //     case 2:
+// //         pacar = "punya pacar 2, aku cukup playboy"
+// //         break
+// //     default:
+// //         pacar = "belum punya pacar"
+// //         break
+// // }
+
+// alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan}kg dan ${pacar}`,)
+// alert(`saldo awal saya sebesar Rp.${saldoAwal} & saldo tambahan yang akan saya miliki sebesar Rp.${saldoTambahan} jadi total saldo yang saya miliki adalah sebanyak Rp.${saldoAkhir}`)
+
+// let namaGuru = ['pak faisol',' bu reny',' pak yoga',' pak ramli']
+// namaGuru.push (' dea',' fiqy')
+// namaGuru.shift()
+// namaGuru.pop()
+// alert (namaGuru)
+
+// for loog
+// const namaGuru = ['Pak yoga', 'Pak ramli']
+// for(let i = 0; i < namaGuru.length; i++) {
+//     console.log(namaGuru[i])
+// }
+
+let i = 0
+
+while (i < 10) {
+    i++
+    console.log('Rafi amin')
+}
